8cd0658d853920289ab189c6f99ae3be7e17a265
Gaytri-Thakre <gaytrithakre8@gmail.com>
2024-05-05 07:53:12 +0530

    User route is added

---
+++ routes/User.js
@@ -0,0 +1,32 @@
+// Import the required modules
+const express = require("express")
+const router = express.Router()
+
+// Import the required controller and middlewares
+const {
+    sendOTP,
+    signUp,
+    login,
+    changePassword
+} = require("../controllers/Auth")
+const {
+    resetPasswordToken,
+    resetPassword,
+} = require("../controllers/ResetPassword")
+
+const {auth} = require("../middleware/auth")
+
+// Routes for login,signup,Authentication
+
+// ********************************************************************************************************
+//                                      Authentication routes
+// ********************************************************************************************************
+router.post("/login",login)
+router.post("/signup",signUp)
+router.post("/sendotp",sendotp)
+router.post("/changepassword",auth,changePassword)
+// ********************************************************************************************************
+//                                      Reset Password
+// ********************************************************************************************************
+router.post("/reset-password-token", resetPasswordToken)
+router.post("/reset-password", resetPassword)
